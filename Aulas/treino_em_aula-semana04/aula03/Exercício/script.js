0c1074fb11e39b67f189ad6ce96fcbb897728dd1
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-03-25 21:22:53 -0300

    add exercicio em aula m1s04-A4

--- Aulas/treino_em_aula-semana04/aula03/Exercício/script.js
+++ Aulas/treino_em_aula-semana04/aula03/Exercício/script.js
@@ -1,49 +1,90 @@
-class Produto {
+class Produto{
     constructor(){
-        this.id = 0;
+        this.id = 1;
         this.arrayProdutos = [];
+
     }
 
-    salvar() {
+    salvar(){
         let produto = this.lerDados();
 
-        if (this.validaCampo(produto)) {
-            alert('salvar');
+        if(this.validaCampos(produto)){
+            this.adicionar(produto);
         }
 
-        console.log(produto);
+        this.listaTabela();
+
     }
 
-    cancelar() {
+    listaTabela(){
+
+        let tbody = document.getElementById('tbody');
+
+        tbody.innerText = '';
+
+        for(let i = 0; i < this.arrayProdutos.length; i++) {
+
+        let tr = tbody.insertRow();
+
+        let td_id = tr.insertCell();
+        let td_produto = tr.insertCell();
+        let td_valor = tr.insertCell();
+        let td_acoes = tr.insertCell();
+
+        td_id.innerText = this.arrayProdutos[i].id;
+        td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+        td_valor.innerText = this.arrayProdutos[i].valor;
+
+        let imgEdit = document.createElement('img');
+        imgEdit.src="./img/editar.jpg";
+
+        td_acoes.appendChild(imgEdit);
+
+        let imgDelete = document.createElement('img');
+        imgDelete.src="./img/deletar.jpg";
+
+        td_acoes.appendChild(imgDelete);
+        }
     }
 
-    lerDados() {
+    adicionar(produto){
+
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados(){
         let produto = {}
 
         produto.id = this.id;
-        produto.nomeProduto = document.getElementById("produto").value;
-        produto.valorProduto = document.getElementById("valor").value;
+        produto.nomeProduto = document.getElementById('produto').value;
+        produto.valor = document.getElementById('valor').value;
 
         return produto;
+
     }
 
-    validaCampo(produto) {
+    validaCampos(produto){
         let msg = '';
 
-        if (produto.nomeProduto == '') {
-            msg += 'Informe um nome de produto! ';
+        if(produto.nomeProduto == ''){
+            msg += ' - Informe o nome do Produto \n';
         }
 
-        if (produto.valorProduto == '') {
-            msg += 'Informe um valor para o produto! ';
+        if(produto.valor == ''){
+            msg += ' - Informe o valor do Produto \n';
         }
 
-        if (msg != '') {
+        if(msg != ''){
             alert(msg);
             return false;
         }
 
         return true;
+    }
+
+    cancelar(){
+        alert('Item cancelado')
     }
 }
 
