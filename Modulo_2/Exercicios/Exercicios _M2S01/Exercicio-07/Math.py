4190c650a1b461e4d91345ef6d2f88088d08e9b0
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-06-12 11:38:06 -0300

    add Exercicio 07 M2S02 Python

---
+++ Modulo_2/Exercicios/Exercicios _M2S01/Exercicio-07/Math.py
@@ -0,0 +1,18 @@
+
+class funcoesMath:
+
+    def __init__(self, n1, n2):
+        self.n1 = n1
+        self.n2 = n2
+
+    def Soma(n1, n2):
+        return n1 + n2
+
+    def Sub(n1, n2):
+        return n1 - n2
+
+    def Div(n1, n2):
+        return n1 / n2
+    
+    def Mult(n1, n2):
+        return n1 * n2
---
+++ Modulo_2/Exercicios/Exercicios _M2S01/Exercicio-07/funcaoMatematica.py
@@ -0,0 +1,60 @@
+# usando while em opções de calculos
+
+from time import sleep
+import Math
+
+print('Para começar o seu cálculo, digite dois valores quaisquer:')
+n1 = float(input('Primeiro número: '))
+n2 = float(input('Segundo número: '))
+
+total = 0
+opcao = 0
+
+while opcao != 6:
+    sleep(1)
+    print(20 * '-=')
+    print('''Agora escolha a opção que você deseja executar: 
+[1] Somar
+[2] Subtrair
+[3] Multiplicar
+[4] Dividir
+[5] Trocar números
+[6] Sair
+''')
+    opcao = int(input('Opção desejada: '))
+    sleep(1)
+
+    if opcao == 1:
+        total = Math.funcoesMath.Soma(n1, n2)
+        sleep(1)
+        print('Calculando...')
+        print('O resultado da soma foi {:.2f}.'.format(total))
+ 
+    elif opcao == 2:
+        total = Math.funcoesMath.Sub(n1, n2)
+        sleep(1)
+        print('Calculando...')
+        print('O resultado da subtração foi {:.2f}.'.format(total))
+
+    elif opcao == 3:
+        total = Math.funcoesMath.Mult(n1, n2)
+        sleep(1)
+        print('Calculando...')
+        print('O resultado da multiplicação foi {:.2f}.'.format(total))
+
+    elif opcao == 4:
+        total = Math.funcoesMath.Div(n1, n2)
+        sleep(1)
+        print('Calculando...')
+        print('O resultado da divisão foi {:.2f}.'.format(total))
+
+    elif opcao == 5:
+        n1 = float(input('Primeiro número: '))
+        n2 = float(input('Segundo número: '))
+
+    elif opcao == 6:
+        print('Finalizando programa...')
+        sleep(1)
+
+sleep(1)
+print(3 * 'Fim -= ')
