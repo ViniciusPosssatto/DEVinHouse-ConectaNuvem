555a77c377e49472e283ab89826d9befb2d8ed30
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-06-24 12:04:32 -0300

    exercicio 03/ semana 04

---
+++ Modulo_2/Exercicios/Exercicios _M2S04/Exercicio-03/main.py
@@ -0,0 +1,33 @@
+from datetime import date
+
+
+class ContaBancaria:
+    def __init__(self, nome: str, cpf: int, anoNascimento):
+        self.nome = nome
+        self.__cpf = cpf
+        self.anoNasc = anoNascimento
+        self.anoAtual = date.today().year
+        self.idade = self.idade_atual()
+        self.__saldo = 0
+        self.limiteSaque = 500
+
+    def idade_atual(self):
+        idade = self.anoAtual - self.anoNasc
+        if idade < 18:
+            print(f'Não é possível criar conta para menores de idade.')
+        else:
+            return idade
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @property
+    def extrato(self):
+        return self.__saldo
+
+pessoa = ContaBancaria('Vini', 402394023, 1994)
+print(pessoa.idade)
+print(pessoa.cpf)
+print(pessoa.extrato)
+pessoa_menor = ContaBancaria('Cau', 234235660, 2006)
