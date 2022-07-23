5c109ab5a2d3f10a674bdbac59012b50ff538bd5
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-07-23 12:20:07 -0300

    ex 04 semana 08

---
+++ Modulo_2/Exercicios/Exercicios_M2S08/Ex-04/ddl_update 2022-07-23 12-19-37.sql
@@ -0,0 +1,26 @@
+-- SELECT GERAL
+SELECT * from universidade.departamentos;
+SELECT * from universidade.cursos;
+SELECT * from universidade.alunos;
+SELECT * from universidade.disciplinas;
+SELECT * from universidade.matriculas;
+
+-- CURSOS
+SELECT * from universidade.cursos;
+
+--ALUNOS
+SELECT nome, data_entrada from universidade.alunos a
+where EXTRACT(YEAR from a.data_entrada)=1950
+SELECT nome, data_entrada from universidade.alunos a
+where EXTRACT(month from a.data_entrada)=04
+
+-- DISCIPLINAS
+SELECT * from universidade.disciplinas
+WHERE carga_horaria >= 30
+
+SELECT nome_disc as "Disciplina", carga_horaria as "Carga horaria" from universidade.disciplinas
+WHERE carga_horaria > 30
+
+-- MATRICULAS
+SELECT mat_aluno, status from universidade.matriculas m
+where m.status='R'
