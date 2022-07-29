57640a800581e0e264f917abad3b59b412abe684
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-07-28 22:15:29 -0300

    add aula 04

--- Modulo_2/Aulas/treino_em_aula-semana09/Aula-04/teste/src/app/controllers/technologies.py
+++ Modulo_2/Aulas/treino_em_aula-semana09/Aula-04/teste/src/app/controllers/technologies.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request
+import re
+from flask import Blueprint, jsonify, request
 from src.app.db import read, save
-from src.app.utils import exist_key
+from src.app.utils import exist_key, exist_value
 
 
 technology = Blueprint('technology', __name__, url_prefix="/technology")
@@ -8,7 +9,9 @@ technology = Blueprint('technology', __name__, url_prefix="/technology")
 
 @technology.route('/', methods=["GET"])
 def list_all_technologies():
-    return {"data": ["Java", "JavaScript", "Python"]}
+    techs = read()
+
+    return jsonify(techs)
 
 
 @technology.route('/', methods=["POST"])
@@ -17,7 +20,41 @@ def list_new_technologies():
 
     data = exist_key(request.get_json(), list_keys)
 
-    print(data)
-    return data
+    if 'error' in data:
+        return jsonify(data), 400
 
+    techs = read()
+    if techs == None or len(techs) == 0:
+        save([data])
+        return jsonify(data), 201
     
+    if exist_value(data, techs):
+        return jsonify({'error': "O id ou tech está repetido no db."}), 400
+    
+    techs.append(data)
+    save(data=techs)
+
+    return data
+    
+
+@technology.route('/<int:id>', methods=["DELETE"])
+def delete_technologies(id):
+
+    techs = read()
+    if techs == None or len(techs) == 0:
+        return {"error": f"O db não contém dados."}, 400
+    
+    only_techs_existent = []
+
+    for data in techs:
+        only_techs_existent.append(data)
+        if data['id'] == id:
+            only_techs_existent.remove(data)
+        else:
+            return {"error": f"Não existe o id = {id}"}, 400
+       
+    print(only_techs_existent)
+    
+    save(only_techs_existent)
+
+    return jsonify(only_techs_existent), 200
