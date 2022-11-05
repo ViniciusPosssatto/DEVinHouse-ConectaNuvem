6fe59c284dabd490720cb308ce0aa56adbdcd622
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-11-05 10:48:01 -0300

    FEAT: exercicio 5 semana 10

--- Modulo_3/Exercicios/Exercicios_M3S10/lab365-base-project-main/src/exercicios/exercicio-5.vue
+++ Modulo_3/Exercicios/Exercicios_M3S10/lab365-base-project-main/src/exercicios/exercicio-5.vue
@@ -1,14 +1,44 @@
 <template>
-  <div class="bg">Espaço para implementar o componente do Exercício 5</div>
+  <div class="bg">
+    <h1>TO do List</h1>
+    <input type="text" placeholder="Digite um item" v-model="item" />
+    <button @click="saveInList()">Insert</button>
+    <div v-if="listaToDo.length === 0">
+      <span>Nenhum conteúdo salvo</span>
+    </div>
+    <div>
+      <ul v-for="(todo, index) in listaToDo" :key="index">
+        <div class="inline">
+          <li v-text="todo"></li>
+          <button @click="deleteItemList(index)">remover</button>
+        </div>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref, reactive } from "vue";
+
 export default {
   name: "ExercicioCinco",
-  data() {
-    return {};
+  setup() {
+    const listaToDo = reactive([]);
+    let item = ref();
+    function saveInList() {
+      listaToDo.push(item.value);
+      this.item = "";
+    }
+    function deleteItemList(item) {
+      listaToDo.splice(item, 1);
+    }
+    return {
+      listaToDo,
+      item,
+      saveInList,
+      deleteItemList,
+    };
   },
-  methods: {},
 };
 </script>
 
@@ -17,5 +47,9 @@ export default {
 .bg {
   background-color: #eee;
   padding: 64px 32px;
+}
+.inline {
+  display: inline-flex;
+  gap: 40px;
 }
 </style>
